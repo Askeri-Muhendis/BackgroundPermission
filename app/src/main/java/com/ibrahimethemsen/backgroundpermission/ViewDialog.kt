873391e219dc6c5e6312bb75d13ac6ce24df4fa5
İbrahim Ethem Şen <ibrahim-ethem-50@hotmail.com>
2023-04-18 13:10:44 +0300

    string

--- app/src/main/java/com/ibrahimethemsen/backgroundpermission/ViewDialog.kt
+++ app/src/main/java/com/ibrahimethemsen/backgroundpermission/ViewDialog.kt
@@ -8,24 +8,26 @@ import android.view.Window
 import android.widget.Button
 
 class ViewDialog {
-    fun showResetPasswordDialog(activity: Activity?,next : () -> Unit,cancel : () -> Unit) {
-        val dialog = Dialog(activity!!).apply {
-            requestWindowFeature(Window.FEATURE_NO_TITLE)
-            setCancelable(false)
-            setContentView(R.layout.alert_dialog)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            findViewById<Button>(R.id.next_btn).apply {
-                setOnClickListener {
-                    next()
-                    dismiss()
+    fun showResetPasswordDialog(activity: Activity?, next: () -> Unit, cancel: () -> Unit) {
+        activity?.let {
+            val dialog = Dialog(activity).apply {
+                requestWindowFeature(Window.FEATURE_NO_TITLE)
+                setCancelable(false)
+                setContentView(R.layout.alert_dialog)
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                findViewById<Button>(R.id.next_btn).apply {
+                    setOnClickListener {
+                        next()
+                        dismiss()
+                    }
                 }
-            }
-            findViewById<Button>(R.id.cancel_btn).apply {
-                setOnClickListener {
-                    cancel()
-                    dismiss()
+                findViewById<Button>(R.id.cancel_btn).apply {
+                    setOnClickListener {
+                        cancel()
+                        dismiss()
+                    }
                 }
-            }
-        }.show()
+            }.show()
+        }
     }
 }
